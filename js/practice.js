9913e0b6fb72d2006ab43d9f5fc76deb12f20417
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2021-01-15 20:01:29 +0200

    fullfilled the task

---
+++ js/practice.js
@@ -0,0 +1,36 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const btnStartRef = document.querySelector('[data-action="start"]');
+const btnStopRef = document.querySelector('[data-action="stop"]');
+let intervalId = null;
+
+btnStartRef.addEventListener('click', btnStartHandler);
+btnStopRef.addEventListener('click', btnStopHandler);
+
+function btnStartHandler() {
+  btnStartRef.disabled = true;
+  btnStopRef.disabled = false;
+  intervalId = setInterval(changeColorHandler, 1000);
+}
+
+function btnStopHandler() {
+ btnStartRef.disabled = false;
+ btnStopRef.disabled = true;
+ clearInterval(intervalId);
+}
+
+function changeColorHandler() {
+  document.body.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length)];
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
